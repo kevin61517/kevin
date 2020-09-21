3ac72ae975d1560ca573ca960f3a3434893b7ace
kevin <yangkaiyun@yangkaiyunde-MacBook-Pro.local>
2020-09-21 23:46:58 +0800

    新增option

--- src/tools/dq_plus.py
+++ src/tools/dq_plus.py
@@ -1,4 +1,8 @@
-import functools, sys
+"""
+This is deque plus, which is design from 69master.
+"""
+import functools
+import sys
 from collections import deque
 from tim_sort import tim_sort
 from typing import Any
@@ -6,29 +10,20 @@ from typing import Any
 __all__ = ["dqlst"]
 
 method_type = type(type("Cls", (), {"method": lambda: 0}))
-method_append = dict()
-method_pop = dict()
+__method_append = dict()
+option_map = {'append': __method_append}
 
 
-def _append(method_key):
+def _method_map(option, method_key):
     def mid(method):
-        method_append.update({method_key: method})
+        option_map[option].update({method_key: method})
 
         @functools.wraps(method)
         def inside(*args, **kws):
             return method(*args, **kws)
+
         return inside
-    return mid
 
-
-def _pop(method_key):
-    def mid(method):
-        method_pop.update({method_key: method})
-
-        @functools.wraps(method)
-        def inside(*args, **kws):
-            return method(*args, **kws)
-        return inside
     return mid
 
 
@@ -36,16 +31,11 @@ class _Implement(deque):
     """
     Deque plus implement.
     """
-
-    def chain_append(self, key: str, *args: Any, **kws: Any) -> object():
-        method_append.get(key, self)(self, *args, **kws)
+    def chain_call(self, option: str, key: str, *args: Any) -> object:
+        option_map[option].get(key, self)(self, *args)
         return self
 
-    def chain_pop(self, key: str, *args: Any, **kws: Any) -> object():
-        method_pop.get(key, self)(self, *args, **kws)
-        return self
-
-    @_append(method_key="right_side")
+    @_method_map(option="append", method_key="right")
     def _a_right(self, *objs: Any) -> None:
         """
         right append
@@ -56,7 +46,7 @@ class _Implement(deque):
         for obj in objs:
             super().append(obj)
 
-    @_append(method_key="left_side")
+    @_method_map(option="append", method_key="left")
     def _a_left(self, *objs: Any) -> None:
         """
         left append
@@ -67,7 +57,7 @@ class _Implement(deque):
         for obj in objs:
             super().appendleft(obj)
 
-    @_append(method_key="both_side")
+    @_method_map(option="append", method_key="both_side")
     def _a_both(self, *objs: Any) -> None:
         """
         left and right
@@ -80,35 +70,24 @@ class _Implement(deque):
             super().append(obj)
             super().appendleft(obj)
 
-    def _p_right(self, *objs: Any):
-        ...
-
     def sort(self):
         """
         :return: sorted data
         """
         return tim_sort(self)
 
-    def print(self, sep=' ', end='\n', file=sys.stdout, flush=False):
-        print(self, sep=sep, end=end, file=file, flush=flush)
+    def print(self, *args, sep=' ', end='\n', file=sys.stdout, flush=False):
+        print(self, *args, sep=sep, end=end, file=file, flush=flush)
         return self
 
 
 class DqList(_Implement):
     """
-    This is deque plus, which is design from 69master.
+    This class used deque of Python module collections.
+    You could used the "chainally calling" of method "chain_call" of this class,
+    chain_call could be assigment any amount of args, but not key words.
+    DqList().chain_call(self, option -> 'append', side -> 'right', *args -> Any)
     """
-    def __str__(self):
-        return """Deque plus"""
-
-    def __repr__(self):
-        return \
-            """
-            This class used deque of Python module collections.
-            You could used the "chain calling" of method "chain_append" of this class,
-            chain_append could be assigment any amount of args, but not key words args.
-            DqList().chain_append()
-            """
 
 
 dqlst = DqList
@@ -116,7 +95,7 @@ dqlst = DqList
 if __name__ == '__main__':
     lst = dqlst
     l = lst([1, 5, 4, 6, 2, 4, 3])
-    ll = l.chain_append("both_side", 0, 0, 0, 0, 0).chain_append("right_side", 100).print()
-    print(l)
+    # ll = l.chain_append("both_side", 0, 0, 0, 0, 0).chain_append("right_side", 100).print()
+    ll = l.chain_call('append', 'right', 999).print('123')
     print(ll)
 
